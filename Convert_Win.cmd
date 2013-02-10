bbbd60f97da68ef2110987f896c87c7e947cff70
evilwombat <evilwombat@server.fake>
2013-02-10 03:57:45 -0800

    JRE path update for Win 64bit (thanks MrCat!)
    Signed-off-by: evilwombat <evilwombat@server.fake>

--- Convert_Win.cmd
+++ Convert_Win.cmd
@@ -1,3 +1,8 @@
 @echo off
-java -jar "%~p0FlightConverter.jar" %*
+
+if exist "C:\Program Files (x86)\Java\jre7\bin\java.exe" (
+	"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar "%~p0FlightConverter.jar" %*
+)	else (
+	java -jar "%~p0FlightConverter.jar" %*
+)
 pause
